1ab694c9c75e8695489454af4bf41cbbfdbc63af
Lina <git@lina.pizza>
2022-09-12 08:47:13 +0000

    github actions, various boilerplate

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -10,16 +10,21 @@ yarn add -D \
   eslint \
   @typescript-eslint/eslint-plugin \
   @typescript-eslint/parser \
+  husky \
   rollup \
   rollup-plugin-dts \
   json \
   npm-add-script
+
+npx husky install
 
 npx npm-add-script -k build -v "tsc && yarn rollup -c"
 npx npm-add-script -k lint -v "eslint src --fix"
 npx npm-add-script -k test -v "jest --config jest.config.ts"
 npx npm-add-script -k preversion -v "yarn test && yarn lint"
 npx npm-add-script -k prepublishOnly -v "yarn test && yarn lint"
+npx npm-add-script -k prepare -v "husky install"
 
+yarn json -I -f package.json -e "this.main=\"dist/bundle.cjs.js\""
 yarn json -I -f package.json -e "this.module=\"dist/bundle.es.js\""
 yarn json -I -f package.json -e "this.types=\"dist/bundle.d.ts\""
