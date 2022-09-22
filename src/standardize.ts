e88f06117c8b67e23628b3e9133e5ec99d3c9360
Lina <git@lina.pizza>
2022-09-22 08:40:35 +0000

    standardization tests

--- src/standardize.ts
+++ src/standardize.ts
@@ -24,87 +24,13 @@ export default function standardize(
     partialDistribution: 'partials',
   }
 ): TSON {
-  const standardized = tson;
-
-  if (tson.tunings && options.tuningSystems === 'tuning systems') {
-    standardized['tuning systems'] = standardized.tunings;
-    delete standardized.tunings;
-  } else if (tson['tuning systems'] && options.tuningSystems === 'tunings') {
-    standardized.tunings = standardized['tuning systems'];
-    delete standardized['tuning systems'];
-  }
-
-  if (standardized[options.tuningSystems]) {
-    for (const tuning of standardized[options.tuningSystems] || []) {
-      for (const scale of tuning.scales) {
-        if (scale.repeat && options.repeatRatio === 'repeat ratio') {
-          scale['repeat ratio'] = scale.repeat;
-          delete scale.repeat;
-        } else if (scale['repeat ratio'] && options.repeatRatio === 'repeat') {
-          scale.repeat = scale['repeat ratio'];
-          delete scale['repeat ratio'];
-        }
-
-        if (scale.min && options.minFrequency === 'min frequency') {
-          scale['min frequency'] = scale.min;
-          delete scale.min;
-        } else if (scale['min frequency'] && options.minFrequency === 'min') {
-          scale.min = scale['min frequency'];
-          delete scale['min frequency'];
-        }
-
-        if (scale.max && options.maxFrequency === 'max frequency') {
-          scale['max frequency'] = scale.max;
-          delete scale.max;
-        } else if (scale['max frequency'] && options.maxFrequency === 'max') {
-          scale.max = scale['max frequency'];
-          delete scale['max frequency'];
-        }
-
-        for (const note of scale.notes) {
-          if (typeof(note) === 'object') {
-            if (note.ratio && options.frequencyRatio === 'frequency ratio') {
-              note['frequency ratio'] = note.ratio;
-              delete note.ratio;
-            } else if (note['frequency ratio'] && options.frequencyRatio === 'ratio') {
-              note.ratio = note['frequency ratio'];
-              delete note['frequency ratio'];
-            }
-          }
-        }
-      }
-    }
-  }
-
-  if (standardized.spectra) {
-    for (const spectrum of standardized.spectra) {
-      if (spectrum.partials && options.partialDistribution === 'partial distribution') {
-        spectrum['partial distribution'] = spectrum.partials;
-        delete spectrum.partials;
-      } else if (spectrum['partial distribution'] && options.partialDistribution === 'partials') {
-        spectrum.partials = spectrum['partial distribution'];
-        delete spectrum['partial distribution'];
-      }
-
-      for (const partial of spectrum[options.partialDistribution] || []) {
-        if (partial.ratio && options.frequencyRatio === 'frequency ratio') {
-          partial['frequency ratio'] = partial.ratio;
-          delete partial.ratio;
-        } else if (partial['frequency ratio'] && options.frequencyRatio === 'ratio') {
-          partial.ratio = partial['frequency ratio'];
-          delete partial['frequency ratio'];
-        }
-
-        if (partial.weight && options.amplitudeWeight === 'amplitude weight') {
-          partial['amplitude weight'] = partial.weight;
-          delete partial.weight;
-        } else if (partial['amplitude weight'] && options.amplitudeWeight === 'weight') {
-          partial.weight = partial['amplitude weight'];
-          delete partial['amplitude weight'];
-        }
-      }
-    }
-  }
-
-  return standardized;
+  console.log(JSON.stringify(tson));
+  return JSON.parse(JSON.stringify(tson)
+    .replace(options.tuningSystems === 'tunings' ? /("tuning systems":)/g : /("tunings":)/g, `"${options.tuningSystems}":`)
+    .replace(options.repeatRatio === 'repeat' ? /("repeat ratio":)/g : /("repeat":)/g, `"${options.repeatRatio}":`)
+    .replace(options.minFrequency === 'min' ? /("min frequency":)/g : /("min":)/g, `"${options.minFrequency}":`)
+    .replace(options.maxFrequency === 'max' ? /("max frequency":)/g : /("max":)/g, `"${options.maxFrequency}":`)
+    .replace(options.frequencyRatio === 'ratio' ? /("frequency ratio":)/g : /("ratio":)/g, `"${options.frequencyRatio}":`)
+    .replace(options.amplitudeWeight === 'weight' ? /("amplitude weight":)/g : /("weight":)/g, `"${options.amplitudeWeight}":`)
+    .replace(options.partialDistribution === 'partials' ? /("partial distribution":)/g : /("partials":)/g, `"${options.partialDistribution}":`));
 }
